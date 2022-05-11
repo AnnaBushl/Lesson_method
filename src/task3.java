8d4f62dea9727a82db81316b260f4d04869757b3
Ann <ann.bushl2444@gmail.com>
2022-05-12 00:05:33 +0300

    Задача 3

--- src/task3.java
+++ src/task3.java
@@ -1,6 +1,6 @@
 public class task3 {
 
-    public static void Distance(int OurDistance) {
+    public static void calculateDeliveryTime(int OurDistance) {
         int time = 1;
         if (OurDistance > 20){
             if (OurDistance > 60 && OurDistance <=100){
@@ -15,7 +15,7 @@ public class task3 {
 
     public static void main(String[] args) {
         int deliveryDistance = 15;    // интервал от [0;20] - 1 день; от (20;60] - 2 дня; от (60;100] - 3 дня
-        Distance(deliveryDistance);
+        calculateDeliveryTime(deliveryDistance);
     }
 
 }
