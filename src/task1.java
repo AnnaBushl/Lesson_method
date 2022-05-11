21ea411cb09daed0b56043bddf55992b323665b7
Ann <ann.bushl2444@gmail.com>
2022-05-12 00:08:33 +0300

    Задача 1

--- src/task1.java
+++ src/task1.java
@@ -1,5 +1,5 @@
 public class task1 {
-    public static void LeapYear(int thisYear) {
+    public static void calculateLeapYear(int thisYear) {
         if ((thisYear%4 == 0 && thisYear%100!=0) || thisYear%400==0 ){
             System.out.println(thisYear + " - високосный год.");
         }else System.out.println(thisYear + " - не високосный год.");
@@ -7,7 +7,7 @@ public class task1 {
 
     public static void main(String[] args) {
         int year = 2021;
-        LeapYear(year);
+        calculateLeapYear(year);
 
     }
 }
